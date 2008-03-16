36a0cbe3302330981bc46d3d96a05a3be084199f
Matthias Lüdtke <github@matthias-luedtke.de>
2008-03-16 14:33:18 +0000

    Output improvements.

--- app/models/tracker.rb
+++ app/models/tracker.rb
@@ -2,6 +2,9 @@ require 'digest/md5'
 
 class Tracker < ActiveRecord::Base
   validates_presence_of :uri, :xpath
+
+  validates_format_of :uri, :with => /^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$/ix
+
   has_many :scrape_results, :order => 'created_at DESC'
 
   def md5sum
@@ -24,5 +27,15 @@ class Tracker < ActiveRecord::Base
    end
 
    dupefree_changes.reverse
+  end
+
+  def last_update
+    return 'now update recognized' if scrape_results_dupefree.empty?
+    scrape_results_dupefree.first.created_at
+  end
+
+  def last_piece
+    return 'now pieces recorded' if scrape_results.empty?
+    scrape_results.first.created_at
   end
 end
