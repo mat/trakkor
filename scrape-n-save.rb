53bed7007bdc2a1144c59c141e00ce83275c19cc
Matthias Lüdtke <github@matthias-luedtke.de>
2008-03-17 15:05:45 +0000

    New scrape-n-save location, added env variable.

---
+++ scrape-n-save.rb
@@ -0,0 +1,28 @@
+require 'yaml'
+require 'logger'
+   
+require "rubygems"
+require "activerecord"
+
+
+require "#{ENV['SCRAPER_PATH']}/app/models/tracker.rb"
+require "#{ENV['SCRAPER_PATH']}/app/models/piece.rb"
+
+LOGFILE = "#{ENV['SCRAPER_PATH']}/log/scrape-n-save.log"
+DATABASE = "#{ENV['SCRAPER_PATH']}/db/dev.sqlite3"
+
+#dbconfig = YAML::load(File.open('/home/mat/svn_workspaces/ruby/scraper/rails/config/database.yml'))  
+#ActiveRecord::Base.establish_connection(dbconfig)  
+
+ActiveRecord::Base.establish_connection(:adapter => "sqlite3", :database => DATABASE)
+
+ActiveRecord::Base.colorize_logging = false
+ActiveRecord::Base.logger = Logger.new(STDERR)  
+ActiveRecord::Base.logger = Logger.new(File.open(LOGFILE, 'a'))
+
+
+trackers = Tracker::find(:all)
+
+trackers.each do |tracker|
+ tracker.fetch_piece.save!
+end
