c16b642582536922f26529019bd24c56895a7999
Matthias Lüdtke <github@matthias-luedtke.de>
2008-03-18 11:53:17 +0000

    Let / point to trackers/

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :pieces
-
+  #map.resources :pieces
   map.resources :trackers
 
   prefix='moduri/'
@@ -31,7 +30,7 @@ ActionController::Routing::Routes.draw do |map|
   #   end
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  # map.root :controller => "welcome"
+  map.root :controller => "trackers"
 
   # See how all your routes lay out with "rake routes"
 
