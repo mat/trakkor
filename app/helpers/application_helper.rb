0c5f0119ca18ce412e7d3a5cfb165e175a2a266d
Matthias Lüdtke <github@matthias-luedtke.de>
2008-04-09 09:17:13 +0000

    Nice titles.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,11 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+ def html_title(str="")
+    unless str.blank?
+      content_for :html_title do
+       "&mdash; #{str} " 
+      end
+    end
+  end  
+
 end
