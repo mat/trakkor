5e336bac53f22b7a1bd3e411cf2ba21539b06e72
Matthias Lüdtke <github@matthias-luedtke.de>
2008-04-11 13:55:51 +0000

    Tidy text before save.

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -7,6 +7,10 @@ class Piece < ActiveRecord::Base
   # # 
   belongs_to :tracker
 
+  def before_save
+    self.text = Piece.tidy_text(self.text)
+  end
+
   def same_content(other)
      return false if other.nil?
  
@@ -16,5 +20,21 @@ class Piece < ActiveRecord::Base
 
   def Piece.html_to_text(html)
     Hpricot(html).inner_text
+  end
+
+
+  def self.tidy_text(str)
+   str = tidy_tabby_lines(str)
+   str = tidy_multiple_nl(str)
+   str = str.strip
+  end
+
+  private
+  def self.tidy_tabby_lines(str)
+    str.gsub(/\n\t+\n/, "\n\n")
+  end
+
+  def self.tidy_multiple_nl(str)
+    str.gsub(/\n\n+/, "\n")
   end
 end
