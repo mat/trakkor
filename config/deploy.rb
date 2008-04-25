7a1e255fb6d1fdb14fd03de704147fb56c7bd66b
Matthias Lüdtke <github@matthias-luedtke.de>
2008-04-25 11:31:54 +0000

    Capified.

---
+++ config/deploy.rb
@@ -0,0 +1,15 @@
+set :application, "mendono"
+set :repository,  "svn://cjs3f.de:4712/ruby/scraper/rails"
+
+# If you aren't deploying to /u/apps/#{application} on the target
+# servers (which is the default), you can specify the actual location
+# via the :deploy_to variable:
+set :deploy_to, "/var/www/#{application}"
+
+# If you aren't using Subversion to manage your source code, specify
+# your SCM below:
+# set :scm, :subversion
+
+role :app, "better-idea.org"
+role :web, "better-idea.org"
+role :db, "better-idea.org", :primary => true
