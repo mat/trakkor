c94e8be360d402f3c12621f666ec06069c8fdbde
Matthias Lüdtke <github@matthias-luedtke.de>
2008-05-16 19:26:35 +0000

    Ported existing tests to Shoulda.

--- test/unit/tracker_test.rb
+++ test/unit/tracker_test.rb
@@ -2,32 +2,44 @@ require File.dirname(__FILE__) + '/../test_helper'
 
 class TrackerTest < ActiveSupport::TestCase
 
-  def setup
-    @new_design = trackers(:new_design_tracker)
+  ############
+  context "A Tracker which recently changed its design" do
+      setup do
+        @tracker = trackers(:new_design_tracker)
+      end
+
+    should "should be sick." do
+      assert @tracker.sick?
+    end
+
+    should "should have some pieces." do
+      assert_equal 51, @tracker.pieces.length
+    end
+
+    should "should have some changes." do
+      assert_equal 2, @tracker.changes.length
+    end
   end
 
-  def test_fresh_tracker_has_no_pieces
-    assert_equal 0, Tracker.new.pieces.length
-  end
+  ############
+  context "A fresh Tracker" do
+      setup do
+        @tracker = Tracker.new
+        @tracker.uri = "http://better-idea.org"
+        @tracker.xpath = "//title"
+      end
 
-  def test_fresh_tracker_has_no_changes
-    assert_equal 0, Tracker.new.changes.length
-  end
+    should "should have no pieces." do
+      assert @tracker.pieces.empty?
+    end
 
-  def test_fresh_tracker_is_not_sick
-    assert !(Tracker.new.sick?)
-  end
+    should "should have no changes." do
+      assert @tracker.changes.empty?
+    end
 
-  def test_sickness_of_tracker_with_new_design
-    assert @new_design.sick?
-  end
-  
-  def test_count_changes_of_tracker_with_new_design
-    assert_equal 2, @new_design.changes.length
-  end
-
-  def test_count_pieces_of_tracker_with_new_design
-    assert_equal 51, @new_design.pieces.length
+    should "should not be sick." do
+      assert !@tracker.sick?
+    end
   end
 
   ############
