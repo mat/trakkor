6a111d77cc0266ac949de0e050e4237efe195c5e
Matthias Lüdtke <github@matthias-luedtke.de>
2008-05-17 11:04:49 +0000

    Add browser profiling via wooza!

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -7,4 +7,23 @@ class ApplicationController < ActionController::Base
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery # :secret => '32f679ed8e66648795a7a54f92f7ae47'
+
+  around_filter do |controller, action|
+    if controller.params.key?("wooza!")
+      require 'ruby-prof'
+  
+      # Profile only the action
+      profile_results = RubyProf.profile { action.call }
+  
+      # Use RubyProf's built in HTML printer to format the results
+      printer = RubyProf::GraphHtmlPrinter.new(profile_results)
+  
+      # Append the results to the HTML response
+      controller.response.body << printer.print("", 0)
+    else
+      action.call
+    end
+  end   
+
+
 end
