ae67584f896e8e45c5943be0cccf60b1668cf5ce
Matthias Lüdtke <github@matthias-luedtke.de>
2009-03-30 10:32:10 +0200

    Render relative dates with jQuery and http://timeago.yarp.com

--- app/helpers/trackers_helper.rb
+++ app/helpers/trackers_helper.rb
@@ -1,5 +1,10 @@
 module TrackersHelper
 
+  def timeago(time, options = {})
+    options[:class] ||= "timeago"
+    content_tag(:abbr, time.to_s, options.merge(:title => time.getutc.iso8601)) if time
+  end
+
   MINUTE = 60
   HOUR   = 60 * 60
   DAY    = 60 * 60 * 24
