0d587a24688fd7fa0c2edf2607ac1e54b1009c97
Matthias Lüdtke <github@matthias-luedtke.de>
2009-03-31 09:50:01 +0200

    Abandon relative_time and remove reference from /examples.

--- app/helpers/trackers_helper.rb
+++ app/helpers/trackers_helper.rb
@@ -9,43 +9,6 @@ module TrackersHelper
   HOUR   = 60 * 60
   DAY    = 60 * 60 * 24
 
-
-  def relative_time(time)
-    return '' if time.nil?
-
-    now = Time.now
-    delta = now - time
-    if (1..90) === delta 
-      return "#{delta.round} seconds ago"
-    end
-
-    if ((1+90)..(MINUTE * 90)) === delta
-      return "#{(delta / MINUTE).round} minutes ago" 
-    end
-    
-    # Prefer 'yesterday' over '19 hours ago'
-    yday = now - DAY
-    if yday.day == time.day && 
-       yday.month == time.month && 
-       yday.year == time.year
-      return "yesterday"
-    end
-
-    if ((1+MINUTE * 90)..(HOUR * 23)) === delta
-      return "#{(delta / HOUR).round} hours ago" 
-    end
-
-    if ((1+DAY * 1)..(DAY * 6)) === delta
-      return time.strftime('last %A') 
-    end
-
-    if time.year == now.year
-      return time.strftime('%A, %d %B')
-    end
-
-    time.strftime('%A, %d %B %Y')
-  end
-
   def bytes_to_human(bytes)
 
     # Shamelessly ported from
